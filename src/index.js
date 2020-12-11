f22ec29f819677172da2d792ae84ba96ffae873e
pacosanchezm <paco.sanchezm@gmail.com>
2020-12-11 08:23:46 -0600

    dic11

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "../eai-order/cs1i";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      id={3872143}
+    />
   </React.StrictMode>,
   rootElement
 );
